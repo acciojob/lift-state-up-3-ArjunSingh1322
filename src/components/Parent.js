60725e1ef7a5e3bf08a21e201ae7e14d35809c6d
Arjun singh <as3388776@gmail.com>
2025-04-09 16:32:54 +0530

    lift3

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,25 +1,20 @@
-import React, {useState} from "react"
-import ChildComponent1 from './ChildComponent1'
+import React, { useState } from "react";
+import ChildComponent1 from "./ChildComponent1";
 import ChildComponent2 from "./ChildComponent2";
 
-const Parent =()=>{
+const App = () => {
+  const [selectedOption, setSelectedOption] = useState('');
 
-    const [value,SetValue] = useState('');
-    console.log(value);
-    return (
-      <div style={{backgroundColor:"rgb(80, 222, 104)"}}>
-          <h1>Parent Component</h1>
-          
-          <ChildComponent1
-            props={SetValue}
-          />
-          <ChildComponent2
-            props={SetValue}
-          />
-          <p>Selected Option:{value}</p>
-      </div>
-    )
-    
-}
+  return (
+    <div className="parent" style={{ backgroundColor: "rgb(80, 222, 104)", padding: "20px" }}>
+      <h1>Parent Component</h1>
 
-export default Parent;
+      <ChildComponent1 onSelect={setSelectedOption} />
+      <ChildComponent2 onSelect={setSelectedOption} />
+
+      <p>Selected Option: {selectedOption}</p>
+    </div>
+  );
+};
+
+export default App;
